be4788fe95d41aeff693c82af470372e8b2b5358
Vadim Reznichenko <vadim.reznichenko@ctdev.io>
2018-12-31 00:35:52 +0200

    init

---
+++ popov Cource CSS/js/equalHeight.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+ function setEqualHeight(columns){
+	 var tallestcolumn = 0;
+	 columns.each(
+		 function() {
+		 currentHeight = $(this).height();
+			 if(currentHeight > tallestcolumn) {
+				tallestcolumn  = currentHeight;
+			 }
+		 }
+	 );
+	 
+	 columns.height(tallestcolumn);
+	 }
+ setEqualHeight($("#sidebarR,#sidebarL,#content"));
+});
